eea25efd19109765f9c618fefe0d1a14ea4447e0
Francisco Furey <franciscofurey@gmail.com>
2023-04-04 08:43:19 -0300

    login form

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -9,6 +13,13 @@ app.config['SECRET_KEY'] = 'SUPER SECRETO'
 todos = ['Comprar Cafe','Enviar Solicitud de compra','Recibir Producto']
 
 
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de Usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
+
+
+    
 @app.errorhandler(404)
 
 def not_found(error):
@@ -25,8 +36,10 @@ def index():
 @app.route('/hello')
 def hello():
     user_ip = session.get('user_ip')
+    login_form = LoginForm()
     context = {
         'user_ip': user_ip,
-        'todos' : todos
+        'todos' : todos,
+        'login_form': login_form
     }
     return render_template('hello.html', **context)
