36dbba9915fc708d982107e66d8d2be6b2780999
Francisco Furey <franciscofurey@gmail.com>
2023-04-04 08:28:40 -0300

    some changs

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
-todos = ['Comprar Cafe',
-         'Enviar Solicitud de compra',
-         'Recibir Producto']
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
+
+todos = ['Comprar Cafe','Enviar Solicitud de compra','Recibir Producto']
 
 
 @app.errorhandler(404)
+
 def not_found(error):
     return render_template('404.html', error = error)
 
@@ -16,14 +18,13 @@ def not_found(error):
 def index():
     user_ip = request.remote_addr
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip',user_ip)
-
+    session['user_ip'] = user_ip
     return response
 
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     context = {
         'user_ip': user_ip,
         'todos' : todos
