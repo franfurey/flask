f836522740eda3121856efe7af9d15c15d27d58f
Francisco Furey <franciscofurey@gmail.com>
2023-04-24 18:48:04 -0300

    blueprints

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
 from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
 from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
 import sys
 import unittest
+from app import create_app
+from app.forms import LoginForm
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
-app.config['SECRET_KEY'] = 'SUPER SECRETO'
+app = create_app()
 
 todos = ['Comprar Cafe','Enviar Solicitud de compra','Recibir Producto']
 
-
-class LoginForm(FlaskForm):
-    username = StringField('Nombre de Usuario', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
 
 app.cli.command()
 def test():
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,7 +1,7 @@
 import unittest
 from flask_testing import TestCase
 from main import app
-from flask import current_app
+from flask import current_app, url_for
 
 
 class MainTest(TestCase):
@@ -14,5 +14,35 @@ class MainTest(TestCase):
     def test_app_exists(self):
         self.assertIsNotNone(current_app)
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_app_in_test_mode(self):
+        self.assertTrue(current_app.config['TESTING'], True)
+
+    # Este de abajo esta dando FAILURE cuando corro tests.
+    def test_index_redirects(self):
+        response = self.client.get(url_for('index'))
+
+        self.assertRedirects(response, url_for('hello'))
+
+    def test_hello_get(self):
+        response = self.client.get(url_for('hello'))
+        self.assert200(response)
+
+    def test_hello_post(self):
+        fake_form = {
+            'username': 'fake',
+            'password': 'fake-pass'
+        }
+        response = self.client.post(url_for('hello'), data=fake_form)
+
+        self.assertRedirects(response, url_for('index'))
+
+    def test_auth_blueprint_exists(self):
+        self.assertIn('auth', self.app.blueprints)
+
+    def test_auth_login_get(self):
+        response = self.client.get(url_for('auth.login'))
+
+        self.assert200(response)
+
+    def test_auth_login_template(self):
+        self.assertTemplateUsed('login.html')
